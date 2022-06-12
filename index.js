2c27c792295eadd80ff02b41df3fea4c6db2eb42
sylvia boyani <sylvia.boyani@student.moringaschool.com>
2022-06-12 09:24:36 +0300

    commit tree 63044ca2ef7775edc1436600edac2ddb6c6aff9b

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Code your solution here
+const findMatching = (drivers, name) => {
+    return drivers.filter(
+      (driver) => driver.toLowerCase() === name.toLowerCase()
+    );
+  };
+  
+  const fuzzyMatch = (drivers, pattern) => {
+    return drivers.filter((driver) => driver.slice(0, 2) === pattern);
+  };
+  
+  const matchName = (drivers, name) => {
+    return drivers.filter((driver) => driver.name === name);
+  };
